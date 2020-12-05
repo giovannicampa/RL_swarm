0b1fdff3df051eefcefccc7e4b82934928850d95
giovannicampa <giocampa93@gmail.com>
2020-12-05 18:23:06 +0100

    Added velocity marker and fixed pose array bug

--- src/swarm_robots/src/simulation_envionment_particles.py
+++ src/swarm_robots/src/simulation_envionment_particles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # In this script we generate the environment of "stupid" particles, that the agent will interact with
-
+from scipy.spatial.transform import Rotation
 import rospy
 from geometry_msgs.msg import PoseArray, PoseStamped
 import numpy as np
@@ -14,13 +14,19 @@ import sys
 sys.path.insert(1, '/home/giovanni/ROS_workspaces/RL_swarm/src/swarm_robots/src/envs')
 from particle_env import ParticleEnv
 
-from stable_baselines import PPO2
-from stable_baselines.common.policies import MlpPolicy
-
-
 
 pub_marker = rospy.Publisher('particles_markers', MarkerArray, queue_size=10)
+pub_marker_velocity = rospy.Publisher('particles_velocity', MarkerArray, queue_size=10)
 pub_poses = rospy.Publisher('particles_positions', PoseArray, queue_size=10)
+
+
+# Generate particles
+particle_list = []
+for i in range(50):
+
+    p = ParticleEnv()
+
+    particle_list.append(p)
 
 
 # Calculating distance between two particles
@@ -28,19 +34,20 @@ def distance_particles(p1, p2):
     return np.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
 
 
-def update_world(particle_list):
+def update_world():
     
 
     while not rospy.is_shutdown():
 
-        markerArray = MarkerArray()
-        poseArray = PoseArray()
+        marker_array = MarkerArray()
+        velocity_marker_array = MarkerArray()
+        pose_array = PoseArray()
 
 
         # Updating all the particles
         for p in particle_list:
 
-            # Checking if there are any close particles
+            # Checking if there are any close by particles
             distance_list = []
             for q in particle_list:
 
@@ -55,18 +62,17 @@ def update_world(particle_list):
             p.closest_particles_y = particle_list[np.argmin(distance_list)].y
             
             p.calc_ang_dist_2_closest()
-            p.update_states()
+            p.update_states_wb()
 
-
+            # Position marker
             marker = Marker()
-            point = PoseStamped()
             marker.header.frame_id = "world"
             marker.id = p.id
             marker.type = marker.SPHERE
             marker.action = marker.ADD
-            marker.scale.x = 2
-            marker.scale.y = 2
-            marker.scale.z = 2
+            marker.scale.x = 4
+            marker.scale.y = 4
+            marker.scale.z = 4
             marker.color.a = 1.0
 
             if(p.collision_path == False):
@@ -82,39 +88,58 @@ def update_world(particle_list):
             marker.pose.position.x = float(p.x)
             marker.pose.position.y = float(p.y)
             marker.pose.position.z = 0
+            marker_array.markers.append(marker)
             
+
+            # Velocity marker
+            velocity_marker = Marker()
+            velocity_marker.header.frame_id = "world"
+            velocity_marker.id = p.id
+            velocity_marker.type = marker.ARROW
+            velocity_marker.action = marker.ADD
+            velocity_marker.scale.x = p.vel*10
+            velocity_marker.scale.y = 1
+            velocity_marker.scale.z = 1
+            velocity_marker.color.a = 1.0
+            velocity_marker.color.r = 1.0
+            velocity_marker.color.g = 0.0
+            velocity_marker.color.b = 0.0
+
+            velocity_marker.pose.position = marker.pose.position
+
+            quat = Rotation.from_euler("z", p.phi, degrees=False).as_quat()
+            velocity_marker.pose.orientation.x = quat[0]
+            velocity_marker.pose.orientation.y = quat[1]
+            velocity_marker.pose.orientation.z = quat[2]
+            velocity_marker.pose.orientation.w = quat[3]
+            velocity_marker_array.markers.append(velocity_marker)
+
+
+
+            # Pose array
+            point = PoseStamped()
             point.header = marker.header
-            point.pose.position.x = marker.pose.position.x
-            point.pose.position.y = marker.pose.position.y
-            point.pose.position.z = marker.pose.position.z
-
-            point.pose.orientation = marker.pose.orientation
-
-            markerArray.markers.append(marker)
-            poseArray.poses.append(point)
-
-        # print(markerArray.markers[1].pose.position.x)
+            point.pose.position.x = float(p.x)
+            point.pose.position.y = float(p.y)
+            point.pose.position.z = 0
+            point.pose.orientation.x = quat[0]
+            point.pose.orientation.y = quat[1]
+            point.pose.orientation.z = quat[2]
+            point.pose.orientation.w = quat[3]
+            pose_array.poses.append(point.pose)
 
 
-        pub_marker.publish(markerArray)
-        pub_poses.publish(poseArray)
+        pub_marker.publish(marker_array)
+        pub_marker_velocity.publish(velocity_marker_array)
+        pub_poses.publish(pose_array)
 
         rospy.sleep(0.01)
 
-
-
-# Generate particles
-particle_list = []
-for i in range(50):
-
-    p = ParticleEnv()
-
-    particle_list.append(p)
 
 
 
 # for each episode, we test the robot for nsteps
 while True:
     rospy.init_node('swarm_node', anonymous=True)
-    update_world(particle_list)
+    update_world()
 
